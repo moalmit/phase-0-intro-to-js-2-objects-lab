f7e173f893cb44e9620b70c8024998653f725d7f
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-03 14:31:27 -0800

    Adds myfile

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "name",
+    streetAddress: "address",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+const newObj = {...obj};
+newObj[key] = value;
+return newObj
+}
+updateEmployeeWithkeyAndValue (employee, "name", "Newddress");
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue (a, key, value){
+    employee[key] = value;
+    return a
+}
+destructivelyUpdateEmployeeWithKeyAndValue (Employee, "newName", "newAddress");
+
+function deleteFromEmployeeByKey (obj, key){
+const newObj = {...obj};
+delete newObj.name;
+return newObj
+}
+deleteFromEmployeeByKey (employee, "Sam",);
+
+function destructivelyDeleteFromEmployeeByKey (obj, key){
+delete obj.name;
+return obj
+}
+destructivelyDeleteFromEmployeeByKey (employee, "name");
+
+
+
